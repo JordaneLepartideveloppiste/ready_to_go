fd21fa2f66e2ce0d0485463c809126c39c1aa441
Jordane <jordanelepart@gmail.com>
2021-07-21 14:30:39 +0200

    initial

--- src/App.js
+++ src/App.js
@@ -1,23 +1,55 @@
-import logo from './logo.svg';
+
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+
+  const [switch1, setSwitch1] = useState(false);
+  const [switch2, setSwitch2] = useState(false);
+  const [switch3, setSwitch3] = useState(false);
+
+const handleClickOn1 = () => {
+  setSwitch1(true);
+}
+const handleClickOff1 = () => {
+  setSwitch1(false);
+}
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="ready_to_go">
+        <div className="buttons">
+          <div id="switch1">
+            <button
+              onClick={handleClickOn1}
+              style={{
+                backgroundColor: switch1 && "purple",
+                color: switch1 ? "white" : "darkgray",
+              }}
+            >
+              ON
+            </button>
+            <button
+              onClick={handleClickOff1}
+              style={{
+                backgroundColor: !switch1 && "purple",
+                color: !switch1 ? "white" : "darkgray",
+              }}
+            >
+              OFF
+            </button>
+          </div>
+          <div id="switch2">
+            <button>ON</button>
+            <button>OFF</button>
+          </div>
+          <div id="switch3">
+            <button>ON</button>
+            <button>OFF</button>
+          </div>
+        </div>
+        <div className="message"></div>
+      </div>
     </div>
   );
 }
